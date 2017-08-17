09cd6e70127daaebdaaf67c6549b91827fca4136
DESKTOP-CG47L7C\KeiSei <keisei.jia@qq.com>
2017-08-17 21:16:58 +0800

    Add vuetify plugin.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import vueTap from 'v-tap'
+import Vuetify from 'vuetify'
 
+Vue.use(Vuetify)
 Vue.use(vueTap)
 
 Vue.config.productionTip = false
