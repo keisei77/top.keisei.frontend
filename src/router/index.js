c3f8257241371565b6df1e90208709e6ed4b0ef0
DESKTOP-CG47L7C\KeiSei <keisei.jia@qq.com>
2017-11-09 22:40:30 +0800

    Add mickey watch.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home/home'
+import Watch from '@/views/watch/watch'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/watch',
+      name: 'watch',
+      component: Watch
     }
   ]
 })
---
+++ src/views/watch/watch.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="mickey-watch">
+    <div class="digit" v-for="i in 12" :key="i">
+      <span>{{ i }}</span>
+    </div>
+    <div id="mickey"></div>
+    <div id="hours" :class="hourClass" :style="hourStyle"></div>
+    <div id="minutes" :class="minutesClass" :style="minutesStyle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      hours: new Date().getHours(),
+      minutes: new Date().getMinutes()
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      this.hours = new Date().getHours()
+      this.minutes = new Date().getMinutes()
+    }, 1000)
+  },
+  computed: {
+    hourClass () {
+      if (this.hours >= 6 && this.hours < 12) {
+        return 'left'
+      }
+      return 'right'
+    },
+    hourStyle () {
+      let deg = this.hours * 31.5
+      if (deg < 180) {
+        deg -= 5
+      }
+      return {
+        transform: `rotate(${deg}deg)`
+      }
+    },
+    minutesClass () {
+      if (this.minutes < 30) {
+        return 'back left'
+      }
+      return 'left'
+    },
+    minutesStyle () {
+      let deg = this.minutes * 6
+      if (deg < 180) {
+        deg += 5
+      } else {
+        deg -= 5
+      }
+      return {
+        transform: `rotate(${deg}deg)`
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'watch';
+</style>
